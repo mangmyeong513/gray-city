63afef719d21d39f7ffa9dacecae9ff24b9590da
mangmyeong513 <mangmyeong513@gmail.com>
2026-03-25 15:11:30 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,37 +1,3 @@
-const CACHE = 'txt-reader-line-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './app.js',
-  './manifest.webmanifest',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-self.addEventListener('install', event => {
+self.addEventListener('install', e=>{
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      const networkFetch = fetch(event.request)
-        .then(response => {
-          const copy = response.clone();
-          caches.open(CACHE).then(cache => cache.put(event.request, copy));
-          return response;
-        })
-        .catch(() => cached || caches.match('./index.html'));
-      return cached || networkFetch;
-    })
-  );
 });
